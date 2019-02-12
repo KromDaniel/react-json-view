ad73637f3ab74c7a28023e2ecfe512b4b14ecfde
Daniel Krom <kromdan@gmail.com>
2019-02-12 18:31:12 +0200

    a

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -10,30 +10,11 @@ const PATHS = {
 
 const config = {
   entry: [PATHS.js + "/index.js"],
-  externals: {
-    cheerio: "window",
-    react: {
-      root: "React",
-      commonjs2: "react",
-      commonjs: "react",
-      amd: "react",
-      umd: "react"
-    },
-    "react-dom": {
-      root: "ReactDOM",
-      commonjs2: "react-dom",
-      commonjs: "react-dom",
-      amd: "react-dom",
-      umd: "react-dom"
-    }
-  },
   output: {
     path: PATHS.build,
     filename: "main.js",
     library: "reactJsonView",
-    libraryTarget: "umd"
   },
-  plugins: [new webpack.optimize.UglifyJsPlugin()],
   resolve: {
     extensions: [".js", ".json", ".css", ".scss"]
   },
